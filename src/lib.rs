f9ac9581d7a8a3179be1c788ced546c5cd39e5ec
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-06-30 08:06:23 -0700

    Adds init function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,19 @@
+use wasm_bindgen::{JsCast, JsValue};
+use wasm_bindgen::closure::Closure;
+use web_sys::HtmlScriptElement;
+
+use crate::browser::document;
+
 pub mod browser;
 pub mod component;
 pub mod entity;
 pub mod scene;
 
+pub fn init(f: impl FnOnce() -> Result<(), JsValue> + 'static) -> Result<(), JsValue> {
+	let document = document();
+	let script: HtmlScriptElement = document.create_element("script")?.dyn_into()?;
+	script.set_attribute("src", "https://aframe.io/releases/1.6.0/aframe.min.js")?;
+	script.add_event_listener_with_callback("load", Closure::once_into_js(f).as_ref().unchecked_ref())?;
+	document.head().expect("Head exists").append_child(&script)?;
+	Ok(())
+}
