5858db0d2dfe45634182f7fbd2669d5c8ee34083
Jeffrey Yu <92849+wehjin@users.noreply.github.com>
2024-06-30 13:42:11 -0700

    Revert aframe script to 1.5.0.

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,12 @@ pub mod browser;
 pub mod component;
 pub mod entity;
 pub mod scene;
+const SCRIPT_URL: &str = "https://aframe.io/releases/1.5.0/aframe.min.js";
 
 pub fn init(f: impl FnOnce() -> Result<(), JsValue> + 'static) -> Result<(), JsValue> {
 	let document = document();
 	let script: HtmlScriptElement = document.create_element("script")?.dyn_into()?;
-	script.set_attribute("src", "https://aframe.io/releases/1.6.0/aframe.min.js")?;
+	script.set_attribute("src", SCRIPT_URL)?;
 	script.add_event_listener_with_callback("load", Closure::once_into_js(f).as_ref().unchecked_ref())?;
 	document.head().expect("Head exists").append_child(&script)?;
 	Ok(())
